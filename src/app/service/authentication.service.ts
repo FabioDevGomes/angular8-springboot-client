dd2bcd937a7ee49c6c49c844ffbdd268d79ff1af
Fábio Dev <fabio.dev.gomes@gmail.com>
2019-10-07 13:42:48 -0300

    ajustando envio de credenciais

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Usuario } from '../model/usuario';
@@ -19,7 +19,16 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<any>('http://localhost:8080/api/v1/validateLogin', { username, password })
+      console.log("============2"+ username +" "+ password);
+
+        return this.http.post<any>('http://localhost:8080/api/v1/validateLogin',  {
+      headers: new HttpHeaders(
+        {
+          'Access-Control-Allow-Origin': 'http://localhost:4200',
+          'Access-Control-Allow-Methods': 'POST',
+          'Authorization': username + password,
+          'Accept': '*/*',
+        })})
             .pipe(map(user => {
                 // store user details and basic auth credentials in local storage to keep user logged in between page refreshes
                 user.authdata = window.btoa(username + ':' + password);
